4a132d5f9ccbf956b526efe7fc62bebb82f13b78
harshitparashar <76948547+harshitparashar@users.noreply.github.com>
2023-12-07 12:16:14 +0530

    Add files via upload
    File addition

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import db as dataBase
+import json 
+
+def connect(client_id):
+     try:
+          client = MongoClient(client_id) #localhost:27017
+          db = client['library'] 
+          collection = db['books'] 
+          return collection
+     except Exception as e:
+          return e     
+     
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_name():
+     return 'hello main page'
+
+@app.route('/get/api/books',methods = ['GET'])
+def get():
+     # try:
+     #      client = MongoClient('mongodb://localhost:27017/') #localhost:27017
+     #      db = client['library'] 
+     #      collection = db['books'] 
+     # except Exception as e:
+     #      return e
+     collection = connect('mongodb://localhost:27017/')
+     ls = []
+     # (1.) returning from mongodb database
+     cursor = collection.find({})
+     for i in cursor:
+          ls.append({
+               'name':i['name'],
+               'author':i['author'],
+               'price':i['price'],
+               'book_id':i['book_id']
+          })
+     
+     # (2.) returning from auxilary python file objects/variable
+     # ls  = dataBase.books
+
+     # (3.) returning from local json file 
+     # f = open('data.json')
+     # ls = json.load(f)
+     # f.close()
+
+     return jsonify(ls)
+
+@app.route('/post/api/books', methods=['POST'])
+def post():
+     try:
+          dat = request.get_json()
+     except Exception as e:
+          return e
+     
+     ds = {
+          'name'    : dat['name'],
+          'author'  : dat['author'],
+          'price'   : dat['price'],
+          'book_id' : dat['book_id']
+     }
+
+     collection = connect('mongodb://localhost:27017/')
+     
+     if collection.count_documents({'book_id':dat['book_id']}) ==0:
+          collection.insert_one(ds)
+          return 'data added for book id : ' + str(ds['book_id'])
+     else:
+          return 'Duplicate entry book_id already exits'
+     
+
+@app.route('/put/api/books/<book_id>',methods=['PUT'])
+def update(book_id):
+     try:
+          dat = request.get_json()
+     except Exception as e:
+          return e
+     
+     collection = connect('mongodb://localhost:27017/')
+     if collection.count_documents({'book_id':book_id }) ==1:
+          try:     
+               collection.update_one({'book_id': book_id},  {"$set":dat})
+               return 'change made to book id : '+ str(book_id)
+          except Exception as e:
+               return e
+     else:
+          return 'non-existant entry ( book_id not found )'
+          
+
+if __name__ == '__main__':
+     app.run(debug=True,port=5000)
